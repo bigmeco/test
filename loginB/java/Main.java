3752bcfea3a587908f7d5c695b70e267613f7ecf
bigmeco <bigmecoprog@gmail.com>
2017-03-16 20:45:54 +0300

    obnov

--- loginB/java/Main.java
+++ loginB/java/Main.java
@@ -7,19 +7,28 @@ import java.sql.*;
  */
 public class Main {
 
-  //  private static final String URL = "jdbc:mysql://localhost:3306/new_schema?autoReconnect=true&useSSL=false";﻿
+    private static final String URLk = "jdbc:mysql://localhost:3306/new_schema?autoReconnect=true&useSSL=false" ;
+    private static final String login = "root" ;
+    private static final String pass = "root" ;
+    private static final String DOBAVLENIE = "INSERT INTO tovar VALUES(?,?,?)" ;
+
 
 
     public static void main(String[] args){
         Connection connection;
-
+        PreparedStatement preparedStatement = null;
         try {
             Driver driver = new FabricMySQLDriver();
             DriverManager.registerDriver(driver);
 
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/new_schema?autoReconnect=true&useSSL=false","root","root");
+            connection = DriverManager.getConnection(URLk,login,pass);
             if(!connection.isClosed()){
                 System.out.println("esti");
+                preparedStatement = connection.prepareStatement(DOBAVLENIE);
+                preparedStatement.setInt(1,0);
+                preparedStatement.setString(2,"egurt");
+                preparedStatement.setInt(3,24);
+            preparedStatement.execute();
                 Statement statement = connection.createStatement();
                 statement.execute("insert into users (name, age, emeil)  values('asds',22,'dfdsfsgvc');");
              // int res =  statement.executeUpdate("update users set name='dd' where id=13;");
@@ -29,7 +38,7 @@ public class Main {
                 statement.addBatch("insert into users (name, age, emeil)  values('asdsdfs',32,'dfdsfsgvc');");
                 statement.addBatch("insert into users (name, age, emeil)  values('asdfs',22,'dfdsfsgvc');");
                 statement.executeBatch();
-                statement.clearBatch();
+               // statement.clearBatch();
             }
             connection.close();
             if(connection.isClosed()){
