4c2813b1a9d3477867afd1b7c06692af0c69cdb9
Nikola <bigmecoprog@gmail.com>
2017-05-07 12:29:15 +0300

    yrok 2

--- RxJava/src/main/java/Main.java
+++ RxJava/src/main/java/Main.java
@@ -2,6 +2,8 @@ import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.sun.activation.registries.LogSupport.log;
 
 /**
@@ -9,6 +11,10 @@ import static com.sun.activation.registries.LogSupport.log;
  */
 public class Main {
     public static void main(String[] args) {
+        longRX ();
+    }
+
+    public void strinRX (){
         Observable<String> observable = Observable.just("one", "two", "three");
         Observer<String> observer = new Observer<String>() {
             @Override
@@ -33,6 +39,65 @@ public class Main {
 
 
         };
+      observable.subscribe(observer);
+    }
+
+    public static void intRX(){
+        Observable<Integer> observable = Observable.range(10, 4);
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Integer s) {
+                System.out.println("onNext: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError: " + e);
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onCompleted");
+            }
+
+
+        };
         observable.subscribe(observer);
     }
+
+    public static void longRX(){
+        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS);
+        Observer<Long> observer = new Observer<Long>() {
+
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe: " + d);
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                System.out.println("onNext: " + aLong);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError: " + e);
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onCompleted");
+            }
+
+
+        };
+        observable.subscribe(observer);
+    }
+
 }
