ee7d63ad9ad4dc487238b81235a11d4fffcb06bb
Nikola <bigmecoprog@gmail.com>
2017-10-03 17:40:02 +0300

    start svoi test

--- TestBT/app/src/main/java/com/example/bigmeco/testbt/MainActivity.java
+++ TestBT/app/src/main/java/com/example/bigmeco/testbt/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.*;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     final static int REQUEST_ENABLE_BT = 1;
+    String status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,22 @@ public class MainActivity extends AppCompatActivity {
         if (bluetooth != null) {
             if (bluetooth.isEnabled()) {
                 // Bluetooth включен. Работаем.
+                String mydeviceaddress= bluetooth.getAddress();
+                String mydevicename= bluetooth.getName();
+                status= mydevicename+" : "+ mydeviceaddress;
+
             } else {
                 // Bluetooth выключен. Предложим пользователю включить его.
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             }
         }
+        Toast.makeText(this, status, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, status, Toast.LENGTH_LONG).show();
     }
 }
