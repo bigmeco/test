781c5fa5a69092635f57558531eb2bd01cb05762
Nikola <bigmecoprog@gmail.com>
2017-10-04 16:33:05 +0300

    start svoi test

--- TestBT/app/src/main/java/com/example/bigmeco/testbt/MainActivity.java
+++ TestBT/app/src/main/java/com/example/bigmeco/testbt/MainActivity.java
@@ -19,7 +19,6 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
     ListView listView;
     final static int REQUEST_ENABLE_BT = 1;
-    final static int CONEXAO = 2;
     String status;
     BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
 
@@ -27,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         if (bluetooth != null) {
             if (bluetooth.isEnabled()) {
@@ -56,54 +54,37 @@ public class MainActivity extends AppCompatActivity {
             }
         }
        // Toast.makeText(this, status, Toast.LENGTH_LONG).show();
-            registerReceiver(discoveryMonitor, new IntentFilter(dStarted));
     }
-    String dStarted = BluetoothAdapter.ACTION_DISCOVERY_STARTED;
-    String dFinished = BluetoothAdapter.ACTION_DISCOVERY_FINISHED;
-    BroadcastReceiver discoveryMonitor;
+
     @Override
     protected void onStart() {
         super.onStart();
-//        Intent List = new Intent(MainActivity.this,Main2Activity.class);
-//        startActivityForResult(List,CONEXAO);
-
-        String aDiscoverable = BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE;
-        startActivityForResult(new Intent(aDiscoverable),
-                0);
-         discoveryMonitor = new BroadcastReceiver() {
-
-
+       // Toast.makeText(this, status, Toast.LENGTH_LONG).show();
+        BroadcastReceiver discoveryResult = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                if (dStarted.equals(intent.getAction())) {
-                    // Процесс обнаружения начался.
-                    Toast.makeText(getApplicationContext(),
-                            "Discovery Started...", Toast.LENGTH_SHORT).show();
-                }
-                else if (dFinished.equals(intent.getAction())) {
-                    // Процесс обнаружения завершился.
-                    Toast.makeText(getApplicationContext(),
-                            "Discovery Completed...", Toast.LENGTH_SHORT).show();
-                }
-            }
+                String remoteDeviceName =
+                        intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
+                BluetoothDevice remoteDevice;
+                remoteDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
+                Toast.makeText(getApplicationContext(),
+                        "Discovered: " + remoteDeviceName,
+                        Toast.LENGTH_SHORT).show();
+                // TODO Сделать что-нибудь с объектом BluetoothDevice.
+            }
         };
 
-
-
+        registerReceiver(discoveryResult, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        if (!bluetooth.isDiscovering())
+            bluetooth.startDiscovery();
     }
-
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-
         // Регистрируем BroadcastReceiver
-       IntentFilter filter=new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(discoveryMonitor, new IntentFilter(dFinished));
-//        registerReceiver(mReceiver, filter);// Не забудьте снять регистрацию в onDestroy
 
     }
 
