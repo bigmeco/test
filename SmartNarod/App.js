2eab8faf0402e13f0aec11de73178819009ee7b2
DESKTOP-PA6B369\bigi <bigmecoprog@gmail.com>
2018-01-23 19:07:44 +0300

    test drawer

--- SmartNarod/App.js
+++ SmartNarod/App.js
@@ -4,60 +4,90 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import Realm from 'realm';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {Component} from "react";
+import Realm from "realm";
+import {Alert, StyleSheet} from "react-native";
+import {Button, Container, Content, Drawer, Footer, FooterTab, Text, ActionSheet, View, Root} from "native-base";
+
+import AppHeader from './AppHeader';
 
 
+import Sidebar from './sidebar';
 export default class App extends Component<{}> {
-	constructor(props) {
-		super(props);
-		this.state = { realm: null };
+    constructor(props) {
+        super(props);
+        this.state = {realm: null};
 
-	}
+    }
 
 
-		componentWillMount() {
-			Realm.open({
-				schema: [{name: 'Dog', properties: {name: 'string'}}]
-			}).then(realm => {
-				realm.write(() => {
-					realm.create('Dog', {name: 'Rex'});
-				});
-				this.setState({ realm });
-			});
-		}
+    componentWillMount() {
+        Realm.open({
+            schema: [{name: 'Dog', properties: {name: 'string'}}]
+        }).then(realm => {
+            realm.write(() => {
+                realm.create('Dog', {name: 'Rex'});
+            });
+            this.setState({realm});
+            realm.addListener('change', updateUI);
 
-		render() {
-			const info = this.state.realm
-				? 'Number of dogs in this Realm: ' + this.state.realm.objects('Dog').length
-				: 'Loading...';
+        });
+    }
 
-			return (
-				<View style={styles.container}>
-					<Text style={styles.welcome}>
-						{info}
-					</Text>
-				</View>
-			);
-		}
-	}
+    componentWillMountq() {
+        Realm.open({
+            schema: [{name: 'Dog', properties: {name: 'string'}}]
+        }).then(realm => {
+            realm.write(() => {
+                realm.create('Dog', {name: 'Rex'});
+            });
+            realm.addListener('change', updateUI);
+
+        });
+    }
+
+    closeDrawer = () => {
+        this.drawer._root.close()
+    };
+    openDrawer = () => {
+        this.drawer._root.open()
+    };
+    render() {
+        return (
+            <Root>
+            <Container>
+                <Drawer
+                    ref={(ref) => { this.drawer = ref; }}
+                    content={<Sidebar/>}
+                    onClose={() => this.closeDrawer()} >
+
+                    <AppHeader
+                        openDrawer={this.openDrawer.bind(this)}
+                    />
+                </Drawer>
+
+        </Container>
+            </Root>
+        );
+    }
+}
+
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
-	},
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
