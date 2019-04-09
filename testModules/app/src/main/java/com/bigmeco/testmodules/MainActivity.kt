95d777afdc01c24d811264cd71a03a8b1d428e1e
bigmeco <bigmecoprog@gmail.com>
2019-04-09 10:07:13 +0300

    test libs

---
+++ testModules/app/src/main/java/com/bigmeco/testmodules/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bigmeco.testmodules
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
